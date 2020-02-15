dea8a2a4eefb1e8c2107144574f4695539069302
Yuri S <fudoshiki.ari@gmail.com>
2020-02-15 22:45:42 +0500

    Update doc

--- lib/sass/compiler.ex
+++ lib/sass/compiler.ex
@@ -6,6 +6,7 @@ defmodule Sass.Compiler do
   @compile {:autoload, false}
   @on_load {:init, 0}
 
+  @doc false
   def init do
     case load_nif() do
       :ok ->
@@ -20,18 +21,28 @@ defmodule Sass.Compiler do
     end
   end
 
+  @doc """
+  Compiles a Sass to CSS
+  """
   def compile(_, _) do
     {:error, :nif_library_not_loaded}
   end
 
+  @doc """
+  Compiles a Sass file to CSS
+  """
   def compile_file(_, _) do
     {:error, :nif_library_not_loaded}
   end
 
+  @doc """
+  LibSass version
+  """
   def version do
     {:error, :nif_library_not_loaded}
   end
 
+  @doc false
   defp load_nif do
     :filename.join(:code.priv_dir(:sass_compiler), 'sass_nif')
     |> :erlang.load_nif(0)
