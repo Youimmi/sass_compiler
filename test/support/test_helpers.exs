0194d7e7d7115a10d7190bafefc856341dabab0a
Yuri S <fudoshiki.ari@gmail.com>
2020-04-21 23:25:22 +0500

    Fix `with` doesn't start with a <- clause

--- test/support/test_helpers.exs
+++ test/support/test_helpers.exs
@@ -16,11 +16,11 @@ defmodule Support.TestHelpers do
   def style_options(:scss, code), do: {"sass", %{output_style: code}}
 
   def compile("", options),
-    do: with({:error, error} = Sass.compile("", options), do: error)
+    do: with({:error, error} <- Sass.compile("", options), do: error)
 
   def compile(content, options),
-    do: with({:ok, css} = Sass.compile(content, options), do: css |> squish)
+    do: with({:ok, css} <- Sass.compile(content, options), do: css |> squish)
 
   def compile_file(path, options),
-    do: with({:ok, css} = Sass.compile_file(path, options), do: css |> squish)
+    do: with({:ok, css} <- Sass.compile_file(path, options), do: css |> squish)
 end
